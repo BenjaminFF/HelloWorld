dc082afda11431578b67d98a375c156608f93ff3
BenjaminFF <990460889@qq.com>
2018-02-26 11:47:44 +0800

    remote4

--- app/src/main/java/com/example/benja/helloworld/SplashActivity.java
+++ app/src/main/java/com/example/benja/helloworld/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.example.benja.helloworld;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable2;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,13 +15,31 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
+        setContentView(R.layout.activity_splash);
+        ImageView imageView=(ImageView)findViewById(R.id.splash_image);
+        AnimatedVectorDrawable avd=(AnimatedVectorDrawable)getResources().getDrawable(R.drawable.splach_anim_vector);
+        imageView.setImageDrawable(avd);
+        avd.start();
+        avd.registerAnimationCallback(new Animatable2.AnimationCallback() {
             @Override
-            public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
+            public void onAnimationStart(Drawable drawable) {
+                super.onAnimationStart(drawable);
             }
-        },2000);
+
+            @Override
+            public void onAnimationEnd(Drawable drawable) {
+                super.onAnimationEnd(drawable);
+                Handler handler=new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },500);
+            }
+        });
+
     }
 }
