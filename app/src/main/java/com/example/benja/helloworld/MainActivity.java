760419444efa90960d213819e4a2846e8c7e248a
BenjaminFF <990460889@qq.com>
2018-03-03 20:23:10 +0800

    remote6

--- app/src/main/java/com/example/benja/helloworld/MainActivity.java
+++ app/src/main/java/com/example/benja/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.benja.helloworld;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,37 +12,70 @@ import android.widget.FrameLayout;
 
 import com.example.benja.helloworld.BottomBar.BottomBar;
 import com.example.benja.helloworld.BottomBar.OnTabSelectedListener;
+import com.example.benja.helloworld.MainFragment.AppFragment;
+import com.example.benja.helloworld.MainFragment.IdeaFragment;
+import com.example.benja.helloworld.MainFragment.StarFragment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private static String TAG="test";
     BottomBar bottomBar;
-    Fragment appFragment,ideaFragment,starFragment;
+    Fragment appFragment,ideaFragment,starFragment,curFragment;
     FragmentManager fragmentManager;
     FrameLayout main_frame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitFragment();
+
         bottomBar=findViewById(R.id.BottomBar);
         bottomBar.setTabSelectedListener(new OnTabSelectedListener() {
             @Override
             public void onTabSelected(int TabId) {
-                switch (TabId){
-                    case R.id.tab_all:
-                        Log.i("test","tab_all");
-                        break;
-                    case R.id.tab_idea:
-                        Log.i("test","tab_idea");
-                        break;
-                    case R.id.tab_star:
-                        Log.i("test","tab_star");
-                        break;
-                }
+                TabSelected(TabId);
             }
         });
 
-        main_frame=findViewById(R.id.main_frame);
 
+    }
+
+    private void TabSelected(int TabId){
+        switch (TabId){
+            case R.id.tab_all:
+                switchFragment(appFragment).commit();
+                break;
+            case R.id.tab_idea:
+                switchFragment(ideaFragment).commit();
+                break;
+            case R.id.tab_star:
+                switchFragment(starFragment).commit();
+                break;
+        }
+    }
+
+    private void InitFragment(){
+        main_frame=findViewById(R.id.main_frame);
+        appFragment=new AppFragment();
+        ideaFragment=new IdeaFragment();
+        starFragment=new StarFragment();
+        curFragment=starFragment;
+        fragmentManager=getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().add(R.id.main_frame,curFragment,curFragment.getClass().getName()).commit();
+
+    }
+
+    private FragmentTransaction switchFragment(Fragment targetFragment){
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        if (!targetFragment.isAdded()){
+            transaction.hide(curFragment);
+            transaction.add(R.id.main_frame,targetFragment,targetFragment.getClass().getName());
+        }else {
+            transaction.hide(curFragment).show(targetFragment);
+        }
+        curFragment=targetFragment;
+        return transaction;
     }
 }
