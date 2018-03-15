ad3c19cca90d0861403aa2c7f5454ff4902e0708
BenjaminFF <990460889@qq.com>
2018-03-15 20:49:17 +0800

    remote6

--- app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/SnapView.java
+++ app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/SnapView.java
@@ -1,21 +1,34 @@
 package com.example.benja.helloworld.SkewMesh.Mesh_Metrix;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 import android.widget.Scroller;
+
+import com.example.benja.helloworld.R;
 
 /**
  * Created by benja on 2018/3/15.
  */
 
-public class SnapView extends LinearLayout{
+public class SnapView extends ViewGroup{
 
     private Scroller mScroller;
 
-    private int Orientation;
+    private int Orientation;  //0表示Vertical 1表示Horizontal
+
+    private int Direction;           //0表示向左，1表示向右
+
+    private int leftBorder,rightBorder;
+
+    private int childindex=0;
 
     public SnapView(Context context) {
         super(context);
@@ -23,6 +36,9 @@ public class SnapView extends LinearLayout{
 
     public SnapView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a=context.getTheme().obtainStyledAttributes(attrs, R.styleable.SnapView,0,0);
+        Orientation=a.getInteger(R.styleable.SnapView_Orientation,0);
+        mScroller=new Scroller(context,new DecelerateInterpolator());
     }
 
     @Override
@@ -38,6 +54,42 @@ public class SnapView extends LinearLayout{
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        super.onLayout(changed, l, t, r, b);
+            int childCount=getChildCount();
+            if (Orientation==0){
+                for(int i=0;i<childCount;i++){
+                    View childView=getChildAt(i);
+                    childView.layout(0,i*childView.getMeasuredHeight(),childView.getMeasuredWidth(),(i+1)*childView.getMeasuredHeight());
+                }
+            }else {
+                for(int i=0;i<childCount;i++){
+                    View childView=getChildAt(i);
+                    childView.layout(i*childView.getMeasuredWidth(),0,(i+1)*childView.getMeasuredWidth(),childView.getMeasuredHeight());
+                }
+            }
+
+            leftBorder = getChildAt(0).getLeft();
+            rightBorder = getChildAt(childCount - 1).getRight();
+
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()){
+            scrollTo(mScroller.getCurrX(),mScroller.getCurrY());
+            invalidate();
+        }
+    }
+
+    public void startScroll(){
+        if (childindex<getChildCount()-1){
+            View childview=getChildAt(childindex);
+            if (Orientation==1){
+                mScroller.startScroll(childview.getLeft(),0,childview.getMeasuredWidth(),0,1000);
+            }else {
+                mScroller.startScroll(0,childview.getTop(),0,childview.getMeasuredHeight(),1000);
+            }
+            invalidate();
+            childindex++;
+        }
     }
 }
--- app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/TranslateFragment.java
+++ app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/TranslateFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.benja.helloworld.R;
 
@@ -24,7 +25,16 @@ public class TranslateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translate, container, false);
+        View v=inflater.inflate(R.layout.fragment_translate, container, false);
+        Button button=v.findViewById(R.id.testButton);
+        final SnapView snapView=v.findViewById(R.id.snapview);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snapView.startScroll();
+            }
+        });
+        return v;
     }
 
 }
