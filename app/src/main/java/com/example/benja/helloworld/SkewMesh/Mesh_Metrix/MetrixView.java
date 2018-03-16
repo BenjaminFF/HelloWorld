8d23a537bfccdccfe4e2c448236a7efb6860f1e8
BenjaminFF <990460889@qq.com>
2018-03-16 16:04:27 +0800

    remote6

--- app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/MetrixView.java
+++ app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/MetrixView.java
@@ -1,6 +1,7 @@
 package com.example.benja.helloworld.SkewMesh.Mesh_Metrix;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,6 +9,8 @@ import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.example.benja.helloworld.R;
 
 /**
  * Created by benja on 2018/3/15.
@@ -23,14 +26,27 @@ public class MetrixView extends View {
     private int mWidth,mHeight;
 
     private Paint paint,pathPaint;
+
+    private int cellWidth;   //cell为正方形 width=height
+
+    private int mbasepointX,mbasePointY;
+
+    private int mSquareWidth;
+
     public MetrixView(Context context) {
         super(context);
     }
 
     public MetrixView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray a=context.getTheme().obtainStyledAttributes(attrs, R.styleable.MetrixView,0,0);
+        row=a.getInteger(R.styleable.MetrixView_row,-1);
+        column=a.getInteger(R.styleable.MetrixView_column,-1);
+
         paint=new Paint();
         paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
         path=new Path();
 
         pathPaint=new Paint();
@@ -44,26 +60,46 @@ public class MetrixView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mWidth=MeasureSpec.getSize(widthMeasureSpec);
         mHeight=MeasureSpec.getSize(heightMeasureSpec);
+
+        if (mWidth>=mHeight){
+            mSquareWidth=mHeight;     //省去多余的部分，让view变成一个正方形，并且居中
+        }else {
+            mSquareWidth=mWidth;
+        }
+        if (row>column){
+            cellWidth=mSquareWidth/row;
+        }else {
+            cellWidth=mSquareWidth/column;
+        }
+
+        mbasepointX=mWidth/2-mSquareWidth/2;
+        mbasePointY=mHeight/2-mSquareWidth/2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.translate(mWidth/2,mHeight/2);
-        if (mWidth>=mHeight){
-            mWidth=mHeight;
+
+        canvas.drawRect(mbasepointX,mbasePointY,mbasepointX+mSquareWidth,mbasePointY+mSquareWidth,pathPaint);
+        canvas.translate(mWidth/2,mHeight/2);    //让canvas原点居中
+        drawOutlinePath(canvas);
+
+    }
+
+    private void drawOutlinePath(Canvas canvas){
+        float dx,dy;
+        if (row%2!=0){
+            dx=(row-1)/2*cellWidth+cellWidth/2;
+        }else {
+            dx=row*cellWidth/2;
         }
-        canvas.translate(-mWidth/2+mWidth/10,-mWidth/2+mWidth/10);
-        canvas.drawRect(0,0,mWidth*8/10,mWidth*8/10,paint);
-        path=new Path();
-        path.moveTo(-mWidth/20+mWidth/10,-mWidth/20);
-        path.lineTo(-mWidth/20,-mWidth/20);
-        path.lineTo(-mWidth/20,mWidth/20+mWidth*8/10);
-        path.lineTo(-mWidth/20+mWidth/10,mWidth/20+mWidth*8/10);
-        path.moveTo(mWidth*8/10+mWidth/20-mWidth/10,-mWidth/20);
-        path.lineTo(mWidth*8/10+mWidth/20,-mWidth/20);
-        path.lineTo(mWidth*8/10+mWidth/20,mWidth/20+mWidth*8/10);
-        path.lineTo(mWidth*8/10+mWidth/20-mWidth/10,mWidth/20+mWidth*8/10);
+        if (column%2!=0){
+            dy=(column-1)*cellWidth/2+cellWidth;
+        }else {
+            dy=column*cellWidth/2;
+        }
+        path.moveTo(0,0);
+        path.lineTo(cellWidth+cellWidth/2,0);
         canvas.drawPath(path,pathPaint);
     }
 }
