235de723e2962e537781eef733cc9e6b34274fcc
BenjaminFF <990460889@qq.com>
2018-03-24 11:38:43 +0800

    remote6

--- app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/CameraFragment.java
+++ app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/CameraFragment.java
@@ -3,9 +3,13 @@ package com.example.benja.helloworld.SkewMesh.Mesh_Metrix;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.benja.helloworld.R;
 
@@ -14,6 +18,7 @@ import com.example.benja.helloworld.R;
  */
 public class CameraFragment extends Fragment {
 
+    ImageView imageView;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -24,7 +29,18 @@ public class CameraFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_camera, container, false);
-
+        imageView=v.findViewById(R.id.myImageView);
+        TextView textView=v.findViewById(R.id.start_rotate_text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Rotate3dAnimation rotate3dAnimation=new Rotate3dAnimation(getContext(),0,360,imageView.getWidth()/2,imageView.getHeight()/2,0,false);
+                rotate3dAnimation.setInterpolator(new LinearInterpolator());
+                rotate3dAnimation.setDuration(3000);
+                imageView.startAnimation(rotate3dAnimation);
+                Log.i("Test",imageView.getHeight()/2+"");
+            }
+        });
         return v;
     }
 
--- app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/Rotate3dView.java
+++ app/src/main/java/com/example/benja/helloworld/SkewMesh/Mesh_Metrix/Rotate3dView.java
@@ -1,6 +1,7 @@
 package com.example.benja.helloworld.SkewMesh.Mesh_Metrix;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Camera;
@@ -12,7 +13,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.Scroller;
 
@@ -22,32 +26,63 @@ import com.example.benja.helloworld.R;
  * Created by benja on 2018/3/21.
  */
 
-public class Rotate3dView extends ImageView{
+public class Rotate3dView extends ViewGroup{
 
-
-    private float degree;
     private Matrix matrix;
-    private Paint paint;
     private Camera camera;
 
+    private float scale;
+
     private int mWidth,mHeight;
-    private Bitmap bitmap;
 
     private Scroller mScroller;
+
+    public enum Orientation{
+        Vertical(0),Horizontal(1);
+
+        int id;
+
+        Orientation(int id) {
+            this.id=id;
+        }
+
+        public static Orientation getOrientation(int id){
+            for(Orientation orientation:values()){
+                if (orientation.id==id) return orientation;
+            }
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private Orientation mOrientation;
+
+    private boolean mLoopable;
+
+    private int leftBorder,rightBorder;
+
     public Rotate3dView(Context context) {
         super(context);
+        initRotate3dView(context);
     }
 
     public Rotate3dView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        initRotate3dView(context);
+
+        TypedArray a=context.getTheme().obtainStyledAttributes(attrs, R.styleable.Rotate3dView,0,0);
+        mOrientation=Orientation.getOrientation(a.getInt(R.styleable.Rotate3dView_Orientation,0));
+        mLoopable=a.getBoolean(R.styleable.Rotate3dView_Loopable,false);
+    }
+
+    private void initRotate3dView(Context context){
         camera = new Camera();
         matrix = new Matrix();
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.RED);
-        bitmap=BitmapFactory.decodeResource(getResources(),R.drawable.android_camera2);
-        this.setScaleType(ScaleType.CENTER_INSIDE);
-        this.setImageBitmap(bitmap);
-        mScroller=new Scroller(context);
+        mScroller=new Scroller(context,new LinearInterpolator());
+        mOrientation=Orientation.Horizontal;
+        mLoopable=false;
+
+        scale = context.getResources().getDisplayMetrics().density;
     }
 
     @Override
@@ -55,31 +90,53 @@ public class Rotate3dView extends ImageView{
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mWidth=MeasureSpec.getSize(widthMeasureSpec);
         mHeight=MeasureSpec.getSize(heightMeasureSpec);
+
+        for(int i=0;i<getChildCount();i++){
+            View childView = getChildAt(i);
+
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childCount=getChildCount();
+        if (mLoopable){
+
+        }else {
+            if (mOrientation==Orientation.Horizontal){
+                leftBorder=0;
+                rightBorder=0;
+                for(int i=0;i<childCount;i++){
+                    View childView=getChildAt(i);
+                    childView.layout(i*childView.getMeasuredWidth(),0,(i+1)*childView.getMeasuredWidth(),childView.getMeasuredHeight());
+                    rightBorder+=(i+1)*childView.getMeasuredWidth();
+                }
+            }else {
+                leftBorder=0;
+                rightBorder=0;
+                for(int i=0;i<childCount;i++){
+                    View childView=getChildAt(i);
+                    childView.layout(0,i*childView.getMeasuredHeight(),childView.getMeasuredWidth(),(i+1)*childView.getMeasuredHeight());
+                    rightBorder+=(i+1)*childView.getMeasuredHeight();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+
+        }
+
+        return super.onTouchEvent(event);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        //setImageDrawable();
-
-        /*matrix.reset();
-        camera.save();
-        camera.translate(0,0,10);
-        camera.rotateY(30);
-        camera.getMatrix(matrix);
-        camera.restore();
-
-        // 修正失真，主要修改 MPERSP_0 和 MPERSP_1
-        float[] mValues = new float[9];
-        matrix.getValues(mValues);			    //获取数值
-        mValues[6] = mValues[6]/5;			//数值修正
-        mValues[7]=mValues[7]/10;
-        matrix.setValues(mValues);			    //重新赋值
-
-        matrix.preTranslate(-mWidth/2,-mHeight/2);
-        matrix.postTranslate(mWidth/2,mHeight/2);*/
-
-        /*canvas.drawBitmap(bitmap,matrix,null);*/
     }
 }
